04199020c35f091e0bc8f501c3bff286db3ed1e6
André Silva <andreluiz.97@outlook.com>
2021-04-19 22:20:20 -0300

    feat: Android authorization

--- packages/nativescript-spotify/index.d.ts
+++ packages/nativescript-spotify/index.d.ts
@@ -1,3 +1,6 @@
 import { NativescriptSpotifyCommon } from './common';
 
-export declare class NativescriptSpotify extends NativescriptSpotifyCommon {}
+export declare class NativescriptSpotify extends NativescriptSpotifyCommon {
+	constructor(clientId: string, redirectUri: string);
+	public async requestAuthorization();
+}
--- tools/demo/nativescript-spotify/index.ts
+++ tools/demo/nativescript-spotify/index.ts
@@ -1,8 +1,16 @@
 import { DemoSharedBase } from '../utils';
-import {} from '@nuxstep/nativescript-spotify';
+import { NativescriptSpotify } from '@nuxstep/nativescript-spotify';
 
 export class DemoSharedNativescriptSpotify extends DemoSharedBase {
-	testIt() {
-		console.log('test nativescript-spotify!');
+	async requestAuthorization() {
+		const spotify = new NativescriptSpotify('b994fed6744b4cf990f41f33cd5d9fc6', 'plugindemo://spotify');
+
+		try {
+			const spotifyAppRemote = await spotify.requestAuthorization();
+			console.log('Authorized');
+			console.log(spotifyAppRemote);
+		} catch (ex) {
+			console.log(`Error: ${ex}`);
+		}
 	}
 }
