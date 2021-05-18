9fa238602d240245b7067ce36e2bbb2e4e0e9fdd
André Silva <andreluiz.97@outlook.com>
2021-05-17 21:46:33 -0300

    fix(spotify): buildArtists index access on Android

--- packages/nativescript-spotify/src/Android/AndroidUtils.ts
+++ packages/nativescript-spotify/src/Android/AndroidUtils.ts
@@ -8,13 +8,13 @@ import { PlayerState } from '../common/PlayerState';
 import { Track } from '../common/Track';
 
 export class AndroidUtils {
-	public static buildArtists(data: any): Array<Artist> {
+	public static buildArtists(data: java.util.ArrayList<Artist>): Array<Artist> {
 		if (!data) return [];
 
 		const artists: Array<Artist> = [];
 
 		for (let i = 0; i < data.size(); i++) {
-			artists.push(new Artist(data[i].name, data[i].uri));
+			artists.push(new Artist(data.get(i).name, data.get(i).uri));
 		}
 
 		return artists;
@@ -22,12 +22,12 @@ export class AndroidUtils {
 
 	public static buildPlayerState(data: any): PlayerState {
 		return new PlayerState(
-			new Track(new Artist(data.track?.artist?.name, data.track?.artist?.uri), this.buildArtists(data.track?.artists), new Album(data.track?.album?.name, data?.track?.album.uri), data.track.duration, data.track.name, data.track.uri, data.track?.imageUri?.raw, data.track.isEpisode, data.track.isPodcast),
-			data.isPaused,
-			data.playbackSpeed,
-			data.playbackPosition,
-			new PlayerOptions(data.playbackOptions?.isShuffling, data.playbackOptions?.repeatMode),
-			new PlayerRestrictions(data.playbackRestrictions?.canSkipNext, data.playbackRestrictions?.canSkipPrev, data.playbackRestrictions?.canRepeatTrack, data.playbackRestrictions?.canRepeatContext, data.playbackRestrictions?.canToggleShuffle, data.playbackRestrictions?.canSeek)
+			new Track(new Artist(data?.track?.artist?.name, data?.track?.artist?.uri), this.buildArtists(data?.track?.artists), new Album(data?.track?.album?.name, data?.track?.album.uri), data?.track?.duration, data?.track?.name, data?.track?.uri, data?.track?.imageUri?.raw, data?.track?.isEpisode, data?.track?.isPodcast),
+			data?.isPaused,
+			data?.playbackSpeed,
+			data?.playbackPosition,
+			new PlayerOptions(data?.playbackOptions?.isShuffling, data?.playbackOptions?.repeatMode),
+			new PlayerRestrictions(data?.playbackRestrictions?.canSkipNext, data?.playbackRestrictions?.canSkipPrev, data?.playbackRestrictions?.canRepeatTrack, data?.playbackRestrictions?.canRepeatContext, data?.playbackRestrictions?.canToggleShuffle, data?.playbackRestrictions?.canSeek)
 		);
 	}
 
